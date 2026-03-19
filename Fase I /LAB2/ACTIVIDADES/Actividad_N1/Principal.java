a895415169898a024f3ecd8370302220de5ffa99
Alexandra Arce <alexandra.arceq@estudiante.ucsm.edu.pe>
2026-03-19 10:53:26 -0500

    Update Principal.java

--- Fase I /LAB2/ACTIVIDADES/Actividad_N1/Principal.java
+++ Fase I /LAB2/ACTIVIDADES/Actividad_N1/Principal.java
@@ -16,3 +16,32 @@ public class Principal {
 		}
 	}
 }
+
+
+//*
+public class Principal {
+	public static void main(String[] args) {
+
+		
+		Bolsa <Chocolatina> bolsaCho = new Bolsa <Chocolatina>(4);
+		Chocolatina c = new Chocolatina("milka");
+		Chocolatina c1 = new Chocolatina("fresa");
+		Chocolatina c2 = new Chocolatina("ferrero");
+		
+		bolsaCho.add(c);
+		bolsaCho.add(c1);
+		bolsaCho.add(c2);
+		
+		for (Chocolatina chocolatina: bolsaCho) {
+			System.out.println(chocolatina.getMarca());
+		}
+		
+		Caja <Chocolatina> cajaChocolatina = new Caja <Chocolatina> ("Rosa");
+		cajaChocolatina.add(c);
+		cajaChocolatina.add(c1);
+		cajaChocolatina.add(c2);
+		
+		cajaChocolatina.mostrar();
+	}
+}
+//*
