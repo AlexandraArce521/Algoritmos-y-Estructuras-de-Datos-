6c9f63ba29fbde42f434b0c3559ae88a70b84d40
Alexandra Arce <alexandra.arceq@estudiante.ucsm.edu.pe>
2026-03-21 23:26:25 -0500

    Create Smartphone.java

---
+++ Fase I /LAB2/EJERCICIOS/Ejercicio_N9/Smartphone.java
@@ -0,0 +1,61 @@
+
+public class Smartphone implements Cargable {
+	
+	private String modelo;
+	private int vatios;
+	private int cantidad;
+	private int nivelBateria;
+	
+	public Smartphone(String modelo, int vatios, int cantidad) {
+		this.modelo = modelo;
+		this.vatios = vatios;
+		this.cantidad = cantidad;
+	}
+	
+	public String getModelo() {
+		return modelo;
+	}
+	
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	
+	public int getVatios() {
+		return vatios;
+	}
+	
+	public void setVatios(int vatios) {
+		this.vatios = vatios;
+	}
+	
+	public int getCantidad() {
+		return cantidad;
+	}
+	
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+	
+	public double getConsumoVatios() {
+		return 0.8;
+	}
+	
+	//No tendra set porque no se puede cambiar por el usuario
+	public int getNivelBateria() {
+		return nivelBateria;
+	}
+	
+	public void cargar(int cantidad) {
+		if(cantidad > 0) { // No se puede colocar una cantidad que baje la bateria 
+			nivelBateria += cantidad;
+			if(nivelBateria > 100) { // Si llega al 100% no cargara más
+				nivelBateria = 100;
+			}
+		}
+	}
+	
+	public void mostrarEstado() {
+		System.out.println("Nivel de bateria: " + nivelBateria +"%\n");
+	}
+
+}
