d610cd02cdb8d185980adab834d56f119b24bbbf
Alexandra Arce <alexandra.arceq@estudiante.ucsm.edu.pe>
2026-03-23 23:53:07 -0500

    Create Bolsa.java

---
+++ Fase I /LAB2/EJERCICIOS/Ejercicio_N1-7/Bolsa.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Bolsa<T> implements Iterable<T> {
+    private ArrayList<T> bolsa = new ArrayList<T>();
+    private int tope;
+
+    public Bolsa(int tope) {
+        this.tope = tope;
+    }
+
+    public void add(T objeto) {
+        if (bolsa.size() < tope) {
+            bolsa.add(objeto);
+        } else {
+            throw new RuntimeException("\n(!)No cabe más");
+        }
+    }
+
+    public Iterator<T> iterador() {
+        return bolsa.iterator();
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return bolsa.iterator();
+    }
+}
