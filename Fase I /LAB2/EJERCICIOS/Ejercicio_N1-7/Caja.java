f93e4240d94ca5500c5901880a45dad1c93bdaf1
Alexandra Arce <alexandra.arceq@estudiante.ucsm.edu.pe>
2026-03-24 10:29:29 -0500

    Update Caja.java

--- Fase I /LAB2/EJERCICIOS/Ejercicio_N1-7/Caja.java
+++ Fase I /LAB2/EJERCICIOS/Ejercicio_N1-7/Caja.java
@@ -1,22 +1,37 @@
-public class Caja <T>{
-	private String color;
-	private T objeto;
-	
-	public Caja(String color, T objeto) {
-		this.color = color;
-		this.objeto = objeto;
-	}
-	
-	public T getObjeto() {
-		return this.objeto;
-	}
-	
-	public String getColor() {
-		return this.color;
-	}
-	
-	@Override
-	public String toString() {
-		return this.objeto.toString();
-	}
+import java.util.ArrayList;
+
+public class Caja<T extends Comparable<T>> {
+    private String color;
+    private T objeto;
+    private ArrayList<T> lista = new ArrayList<>();
+
+    public Caja(String color, T objeto) {
+        this.color = color;
+        this.objeto = objeto;
+    }
+
+    public T getObjeto() {
+        return this.objeto;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public void add(T objeto) {
+        lista.add(objeto);
+    }
+
+    public ArrayList<T> getLista() {
+        return lista;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Caja color: ").append(color).append("\n");
+        sb.append("Objeto principal: ").append(objeto).append("\n");
+        sb.append("Lista de objetos: ").append(lista);
+        return sb.toString();
+    }
 }
