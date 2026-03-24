c41953a455ab48e034d067487300b85cfcf135b8
Alexandra Arce <alexandra.arceq@estudiante.ucsm.edu.pe>
2026-03-23 23:16:47 -0500

    Create Golosina.java

---
+++ Fase I /LAB2/EJERCICIOS/Ejercicio_N1-7/Golosina.java
@@ -0,0 +1,45 @@
+public class Golosina {
+	private String nombre;
+	private double peso;
+	
+	public Golosina(String nombre, double peso) {
+		this.nombre = nombre;
+		this.peso = peso;
+	}
+	
+	public String getNombre() {
+		return this.nombre;
+	}
+	
+	public double getPeso() {
+		return this.peso;
+	}
+	
+	public void setPeso(double peso) {
+		this.peso = peso;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if(this == obj)
+			return true;
+		
+		if(obj == null)
+			return false;
+		
+		if(!(obj instanceof Golosina))
+			return false;
+		
+		Golosina g = (Golosina) obj;
+		return this.nombre.equals(g.getNombre()) && this.peso == g.getPeso();
+	}
+	
+	@Override
+	public String toString() {
+		return "Golosina: " + this.nombre + ", peso: " + this.peso;
+	}
+}
