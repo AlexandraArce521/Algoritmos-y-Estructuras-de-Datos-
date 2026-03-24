56b3d9a65cc91ee66287ba4f5df61d844b371655
Alexandra Arce <alexandra.arceq@estudiante.ucsm.edu.pe>
2026-03-24 09:43:04 -0500

    Update Laptop.java

--- Fase I /LAB2/EJERCICIOS/Ejercicio_N9/Laptop.java
+++ Fase I /LAB2/EJERCICIOS/Ejercicio_N9/Laptop.java
@@ -2,7 +2,6 @@
 public class Laptop implements Cargable {
 	private String marca;
 	private int vatios;
-	private int cantidad;
 	private int nivelBateria;	
 	
 	
@@ -27,14 +26,6 @@ public class Laptop implements Cargable {
 	
 	public void setVatios(int vatios) {
 		this.vatios = vatios;
-	}
-	
-	public int getCantidad() {
-		return cantidad;
-	}
-	
-	public void setCantidad(int cantidad) {
-		this.cantidad = cantidad;
 	}
 	
 	public double getConsumoVatios() {
