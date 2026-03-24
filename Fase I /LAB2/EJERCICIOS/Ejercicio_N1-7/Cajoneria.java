6a3fede2fe464a4fbbb1e4dd846dd3c12c04d4cd
Alexandra Arce <alexandra.arceq@estudiante.ucsm.edu.pe>
2026-03-23 23:15:33 -0500

    Create Cajoneria.java

---
+++ Fase I /LAB2/EJERCICIOS/Ejercicio_N1-7/Cajoneria.java
@@ -0,0 +1,63 @@
+import java.util.ArrayList;
+
+public class Cajoneria<T> {
+	private ArrayList<Caja<T>> caja = new ArrayList<Caja<T>>();
+	private int tope;
+	
+	public Cajoneria(int tope) {
+		this.tope = tope;
+	}
+	
+	public void add(Caja<T> objeto) {
+		if (caja.size() < tope) {
+			caja.add(objeto);
+		} else {
+			throw new RuntimeException("no caben mas");
+		}
+	}
+	
+	public String search(T elemento) {
+		for(int i = 0; i < caja.size(); i++) {
+			if(caja.get(i).getObjeto().equals(elemento)) {
+				return i + " " + caja.get(i).getColor();
+			}
+		}
+		return "No existe";
+	}
+	
+	public T delete(T elemento) {
+		for(int i = 0; i < caja.size(); i++) {
+			if(caja.get(i).getObjeto().equals(elemento)) {
+				T eliminado = caja.get(i).getObjeto();
+				caja.remove(i);
+				return eliminado;
+			}
+		}
+		return null;
+	}
+	
+	@Override
+	public String toString() {
+		String mensaje;
+		mensaje = "Posicion    Color Caja    Objeto\n";
+		mensaje += "_________________________________\n";
+		for(int i = 0 ; i < caja.size(); i++) {
+			mensaje += " " + i + "   " + caja.get(i).getColor() + "      " + caja.get(i) + "\n";
+		}	
+		return mensaje;
+	} 
+	
+	public int contar(T elemento) {
+		int contador = 0;
+		for(int i = 0; i < caja.size(); i++) {
+			if(caja.get(i).getObjeto().equals(elemento)) {
+				contador += 1;
+			}
+		}
+		return contador;
+	}
+	
+	public ArrayList<Caja<T>> getCaja(){
+		return this.caja;
+	}
+}
