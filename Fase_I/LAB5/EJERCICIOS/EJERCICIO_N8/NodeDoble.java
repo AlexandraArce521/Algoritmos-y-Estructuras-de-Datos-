93e3b557ea8671f170d6bdf8b1e3381f4397d0cb
Alexandra Arce <alexandra.arceq@estudiante.ucsm.edu.pe>
2026-04-27 23:57:23 -0500

    Create NodeDoble.java

---
+++ Fase_I/LAB5/EJERCICIOS/EJERCICIO_N8/NodeDoble.java
@@ -0,0 +1,12 @@
+public class NodeDoble<T> {
+	T dato;
+	NodeDoble<T> next;
+	NodeDoble<T> prev;
+	
+	public NodeDoble(T dato) {
+        this.dato = dato;
+        this.next = null;
+        this.prev = null;
+    }
+	
+}
