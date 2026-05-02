21e81c462295654a35bffa2070a805cde2fc6c19
Alexandra Arce <alexandra.arceq@estudiante.ucsm.edu.pe>
2026-05-02 00:33:45 -0500

    Update Queue.java

--- Fase_I/LAB_N6/ACTIVIDADES/Queue.java
+++ Fase_I/LAB_N6/ACTIVIDADES/Queue.java
@@ -1,1 +1,11 @@
-
+class ExceptionIsEmpty extends Exception {
+    public ExceptionIsEmpty(String mensaje) {
+        super("(!)Cola vacia");
+    }
+}
+public interface Queue<E> { 
+    void enqueue(E x); 
+    E dequeue() throws ExceptionIsEmpty; 
+    E front() throws ExceptionIsEmpty; 
+    boolean isEmpty(); 
+} 
