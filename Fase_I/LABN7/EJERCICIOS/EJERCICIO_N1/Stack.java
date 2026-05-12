71019f74651e7fd7dcaecedad576d09153fb4d15
Alexandra Arce <alexandra.arceq@estudiante.ucsm.edu.pe>
2026-05-12 10:25:35 -0500

    Delete Fase_I/LABN7/EJERCICIOS/EJERCICIO_N1/Stack.java

--- Fase_I/LABN7/EJERCICIOS/EJERCICIO_N1/Stack.java
+++
@@ -1,9 +0,0 @@
-package Pilas;
-import Exceptions.ExceptionIsEmpty;
-
-public interface Stack<E> {
-	void push(E x); 
-	E pop() throws ExceptionIsEmpty; 
-	E top() throws ExceptionIsEmpty; 
-	boolean isEmpty();
-}
