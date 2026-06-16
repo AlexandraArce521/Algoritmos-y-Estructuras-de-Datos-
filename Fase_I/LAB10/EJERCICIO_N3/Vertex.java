d5854698b7b05fe7f44f49a344351f915e74c01b
Alexandra Arce <alexandra.arceq@estudiante.ucsm.edu.pe>
2026-06-15 22:50:04 -0500

    Create Vertex.java

---
+++ Fase_I/LAB10/EJERCICIO_N3/Vertex.java
@@ -0,0 +1,12 @@
+package EJERCICIO_03;
+
+public class Vertex<V> {
+	V data;
+    public Vertex(V data) {
+        this.data = data;
+    }
+
+    public V getData() {
+        return data;
+    }
+}
